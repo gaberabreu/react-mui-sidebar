8b4b55dce40606c59138505a27f29a28e5afd814
gaberabreu <gaberabreu.github@gmail.com>
2024-11-14 20:02:52 -0300

    Change project to be a lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      input: {
+        main: resolve(__dirname, "src/main.ts"),
+      },
+      output: {
+        entryFileNames: "main.js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 });
