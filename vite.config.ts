d9c7bd6f5c8c811f0c424013f29516401a767bce
gaberabreu <gaberabreu.github@gmail.com>
2024-11-14 20:02:52 -0300

    Initial vite as lib project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        entryFileNames: "[name].js",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
