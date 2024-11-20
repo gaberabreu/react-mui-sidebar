0319f5aa3c1a9e1a27e7f06fe2d783a5af07f9c1
gaberabreu <gaberabreu.github@gmail.com>
2024-11-19 21:24:20 -0300

    Add types to be exported too

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 export { default as MenuItem } from "./components/MenuItem";
+export type { MenuItemProps } from "./components/MenuItem";
+
 export { default as MenuSubHeader } from "./components/MenuSubHeader";
+export type { MenuSubHeaderProps } from "./components/MenuSubHeader";
+
 export { default as SideBar } from "./components/SideBar";
+export type { SideBarProps } from "./components/SideBar";
+
 export { default as SubMenu } from "./components/SubMenu";
+export type { SubMenuProps } from "./components/SubMenu";
